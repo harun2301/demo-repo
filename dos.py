6be3d5918d978eae44665232be34bd7240c1e10c
harun2301 <oscar_harun@comunidad.unam.mx>
2022-10-06 01:06:53 -0500

    second commit on branch
    dos.py modified, added conditionals

--- dos.py
+++ dos.py
@@ -1,3 +1,11 @@
 # this script was added on branch "prueba_1"
 
 print("probando cómo chingados funciona el git")
+
+var = [1, 2, 3, 5]
+
+if 4 in var:
+    print("sí hay 4")
+
+else:
+    print("no hay 4")
